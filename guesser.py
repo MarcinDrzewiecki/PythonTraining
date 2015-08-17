9931b0078a0c2ef5803b409ad73378ce693930e4
Marcin Drzewiecki <drzewko@localhost.localdomain>
2015-08-17 18:29:04 +0200

    initial commit

---
+++ guesser.py
@@ -0,0 +1,27 @@
+__author__ = 'drzewko'
+import random
+dictionary = ("slowo", "glupstwo","mama","powiesc","pieniadz","milosc","wladza")
+
+guess_the_word = random.choice(dictionary)
+print guess_the_word
+num_of_words=len(guess_the_word)
+tries = 5
+
+print "Guess the word containing",num_of_words, "letters."
+print "You have",tries,"tries"
+
+for i in range(tries):
+    letter_guess=raw_input("What letter is in this word?\n")
+    if letter_guess in guess_the_word:
+        print "Yes!!"
+    else:
+        print "Nope :("
+    print "You have",tries-i-1, "available letter guesses left"
+
+user_guess = raw_input("\nNow guess the word\n")
+if user_guess==guess_the_word:
+    print "Brawo!"
+else:
+    print "Sorry, not this time"
+
+raw_input("\nPress ENTER to finish the game")
---
+++ reverse_word.py
@@ -0,0 +1,9 @@
+__author__ = 'drzewko'
+
+word = raw_input("Input a word\n")
+word_length=len(word)
+reverse_word=""
+for i in range(word_length):
+    reverse_word+=word[word_length-i-1]
+
+print reverse_word
