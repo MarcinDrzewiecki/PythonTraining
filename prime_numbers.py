855ad63ff0565d045afc4fe46a745d01ac571a6e
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-08-19 09:22:03 +0200

    prime numbers refactor

--- prime_numbers.py
+++ prime_numbers.py
@@ -6,16 +6,16 @@ interval = int(raw_input("Prime number scanner will detect and add to each other
 prime_numbers = []
 
 
-def prime_number(number):
+def is_prime(number):
     for x in range(2, number):
         if number % x == 0:
-            break
+            return False
     else:
-        return number
+        return True
 
 for x in range(2, interval+1):
-    prime_numbers.append(prime_number(x))
+    if is_prime(x):
+        prime_numbers.append(x)
 
-prime_numbers=filter(None,prime_numbers)
 print(prime_numbers)
 print(sum(prime_numbers))
