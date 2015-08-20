343707ec2fa3b3f163e706d4069f42d0ec2e1c4d
drzewko <marcin.drzewiecki@gmail.com>
2015-08-20 07:12:25 +0200

    changing prime numbers program

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,8 +1,10 @@
 __author__ = 'drzewiec'
 
 interval = int(raw_input("Prime number scanner will detect and add to each other all prime numbers from 2 to: "))
+how_many_prime_numbers = int(raw_input("How many prime numbers should i add to each other? :  "))
 
 prime_numbers = []
+count=0
 
 
 def is_prime(number):
@@ -11,9 +13,18 @@ def is_prime(number):
             return False
     return True
 
-for x in range(2, interval + 1):
+for x in range(2,1000000):
     if is_prime(x):
         prime_numbers.append(x)
+        count += 1
+        if count == how_many_prime_numbers:
+            break
+
+for x in range(2, interval + 1):
+   if is_prime(x):
+      prime_numbers.append(x)
 
 print(prime_numbers)
 print(sum(prime_numbers))
+
+
