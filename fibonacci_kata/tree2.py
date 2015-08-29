79b861ed6c50cebab7ee0a451310438b9e4d8ece
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-08-29 10:10:36 +0200

    training with Cepe

---
+++ fibonacci_kata/tree2.py
@@ -0,0 +1,26 @@
+__author__ = 'drzewiec'
+
+
+
+
+class Node(object):
+    def __init__(self,v,l,r):
+        self.r = r
+        self.v = v
+        self.l = l
+    def h(self):
+        return 1+max(self.l.h(),self.r.h())
+
+class Nil(object):
+    def h(self):
+        return 0
+
+
+node0 = Nil()
+print node0.h()
+
+node1 = Node(2,Node(3,Nil(),Node(5,Nil(),Node(1,Nil(),Nil()))),Node(-1,Nil(),Nil()))
+
+print node1.h()
+print node1.s()
+#policzyc sume wszystkich wartosci
