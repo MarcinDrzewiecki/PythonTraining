6a801acf430d38408c707e4adb5297a75af128c2
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-08-31 08:18:29 +0200

    training with Cepe folder refactor

--- azz.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'drzewko'
-
-
-def zipvalidate(postcode):
-    if len(postcode) != 6 or not postcode.isdigit() or postcode[0]=="0" or postcode[0]=="5" or postcode[0]=="7" or postcode[0] == "8" or postcode[0] == "9":
-        return False
-    return True
-
-print zipvalidate('023222')
