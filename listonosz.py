2cce85a7daa22641809aba07f449fa2e9d35aa8d
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-10-27 11:03:49 +0100

    listonosz task

--- listonosz.py
+++ listonosz.py
@@ -13,19 +13,19 @@ visited_cities =[]
 ile_miast = int(input("How many cities? "))
 
 for x in range(0,ile_miast):
-    all_cities.append(list(input("podaj wspolzedne miast ")))
+    all_cities.append(list(input("Set the coordinates for city "+ str(x+1)+":\n")))
 
-cities = list(input("Ktore miasta"))
+cities = list(input("To which cities the mailman will travel?\n"))
 for x in cities:
-    visited_cities.append(all_cities[x])
+    visited_cities.append(all_cities[x-1])
 ile_miast_zwiedzonych = len(visited_cities)
 
-def droga_przebyta(visited_cities):
-    starting_point = visited_cities[0]
+def droga_przebyta(city):
+    starting_point = city[0]
     road = 0
     for x in range(0,ile_miast_zwiedzonych):
-        road = road + dlugosc(starting_point,visited_cities[x])
-        starting_point=visited_cities[x]
-    return road + dlugosc(visited_cities[0],starting_point)
+        road = road + dlugosc(starting_point,city[x])
+        starting_point=city[x]
+    return road + dlugosc(city[x],city[0])
 
 print(droga_przebyta(visited_cities))
